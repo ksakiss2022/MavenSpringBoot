a5a5ac20a0d0fbaf3969b647d77d040ac323c519
Swetlana <sweta_zaika@mail.ru>
2022-11-26 14:26:44 +0300

    домашка Maven Spring Boot

---
+++ src/main/java/com/skyprotwoo/employeebookspringtwo/EmployeeBookSpringTwoApplication.java
@@ -0,0 +1,13 @@
+package com.skyprotwoo.employeebookspringtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookSpringTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBookSpringTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skyprotwoo/employeebookspringtwo/EmployeeBookSpringTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyprotwoo.employeebookspringtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookSpringTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
